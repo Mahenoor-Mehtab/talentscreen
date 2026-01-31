67c74b180b9fab2c545311709e4650a020e15f70
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2026-01-31 23:35:15 +0530

    01-backend-setUp

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
+import {ENV} from './lib/env.js'
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+// ES Module __dirname fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -11,11 +18,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({msg:'Server is running on 3000'});
 });
 
+app.get('/books',(req, res)=>{
+      res.json({msg:'this is the book endpoint'});
+})
 
+// make out app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    const rootDir = path.resolve(__dirname, '../../');
+
+  const frontendPath = path.join(rootDir, 'frontend/dist');
+
+   console.log('Serving frontend from:', frontendPath);
+
+    app.use(express.static(frontendPath));
+
+    app.get('*any', (req, res) => {
+        res.sendFile(path.join(frontendPath, 'index.html'));
+    });
+}
 
 // Start server
 const PORT = process.env.PORT || 5000;
