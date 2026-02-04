e2799fbddf84159171403a966a089cdd4fc934cd
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2026-02-04 17:38:42 +0530

    fix-error

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import connectDB from "./db";
+import connectDB from "./db.js";
 import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "talent-screen" }); // created inngest client = allows use to communicate with inngest
